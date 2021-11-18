49f740d21ee6c5fc2e66dcf21fcc27cfd9e98044
Justin Schulberg <45433981+jschulberg@users.noreply.github.com>
2021-11-18 11:45:35 -0500

    Code to bring in DOB and Adoption Date
    These two fields, which are generally available for *most* dogs, can be used to calculate the age of the dog

--- Code/ReadAdoptionData.py
+++ Code/ReadAdoptionData.py
@@ -18,8 +18,11 @@ def read_in_dog_list(file, year):
     for each month of the year).
     
     INPUTS:
-        file | name of the file within the data folder
-        year | string for the year this corresponds to
+        file | Name of the file within the data folder
+        year | String for the year this corresponds to (i.e. '2020')
+    OUTPUTS:
+        df | Singular DataFrame with all of our data for the year in a 
+             consolidated format
     '''
     # Read in all sheets as an ordered dictionary
     sheets = pd.read_excel('Data/' + file, sheet_name = None)
@@ -51,15 +54,20 @@ def read_in_dog_list(file, year):
 
 #%%
 for file in os.listdir("Data"):
-    if '2021' in file:
+    if 'DOG LIST 2020' in file:
         print(f"Reading in {file}...")
         # Read in CSV and skip first row
         dog_list_2021 = read_in_dog_list(file, '2021')
 
-    if '2020' in file:
+    elif 'DOG LIST 2021' in file:
         print(f"Reading in {file}...")
         # Read in CSV and skip first row
         dog_list_2020 = read_in_dog_list(file, '2020')
+        
+    elif 'DOG LIST 2019' in file:
+        print(f"Reading in {file}...")
+        # Read in CSV and skip first row
+        dog_list_2019 = read_in_dog_list(file, '2019')
         
     elif 'Archived' in file:
         print(f"Reading in {file}...")
@@ -85,14 +93,14 @@ for file in os.listdir("Data"):
             
 
 #%% Rename like columns
-dog_list_2020 = dog_list_2020.rename(columns = {'ID #': 'ID'})
 dog_list_2021 = dog_list_2021.rename(columns = {'GENDER': 'SEX'})
 dog_list_archive = dog_list_archive.rename(columns = {'LBS': 'WEIGHT',
                                                       'PRIMARY BREED': 'BREED MIXES',
                                                       'Petfinder Link': 'LINK'})
         
+
 #%% Concat everything together
-dog_list_all = pd.concat([dog_list_2020, dog_list_2021, dog_list_archive], sort = False)
+dog_list_all = pd.concat([dog_list_2019, dog_list_2020, dog_list_2021, dog_list_archive], sort = False)
 
 # Coalesce columns that should be the same (i.e. 'ID #' and 'ID')
 dog_list_all['ID'] = dog_list_all['ID'].combine_first(dog_list_all['ID #'])
@@ -107,6 +115,7 @@ dog_list_all['BEHAVIORAL NOTES'] = dog_list_all['BEHAVIORAL NOTES'].combine_firs
                 
 print(dog_list_all.year.value_counts())
 print(dog_list_all.month.value_counts())
+
 
 #%% Remove potentially sensitive columns related to the adopter
 # and other columns we don't need
@@ -130,5 +139,99 @@ dog_list_deduped = dog_list_dropped.copy().drop_duplicates() \
 print(dog_list_deduped.year.value_counts())
 print(dog_list_deduped.month.value_counts())
 
+
+
+#%%##################################
+##### Bring in DOB and Adoption Dates
+#####################################
+# NOTE: Adoption dates are located across two major datasets
+# We only have DOB for 2020/2021 unfortunately
+dob_2020 = pd.read_excel('Data/animal_export_2020.xlsx')
+dob_2021 = pd.read_excel('Data/animal_export_2021.xlsx')
+dob_2016_2019 = pd.read_excel('Data/animal_export_2016-2019.xlsx')
+adoption_date = pd.read_excel('Data/adoption date to ID mapping.xlsx')
+adoption_date_all = pd.concat(pd.read_excel('Data/Lucky Dog Adoption Spreadsheet.xlsx', sheet_name = None), ignore_index = False)
+
+
+#%% Only pull out the columns we want and drop dupes
+all_dobs = pd.concat([dob_2020, dob_2021, dob_2016_2019], sort = False)[['Animal ID', 'Date of Birth']] \
+            .drop_duplicates() \
+            .rename(columns = {'Animal ID': 'ID'})
+
+adoption_cleaned = adoption_date.loc[adoption_date['Species'] == 'Dog', ['ID', 'Adopted On']] \
+                                .drop_duplicates()
+
+
+# Fix up the ID columns
+all_dobs['ID'] = all_dobs['ID'].str.strip()
+adoption_cleaned['ID'] = adoption_cleaned['ID'].str.strip()
+
+
+
+#%% Join everything together
+all_dates = pd.merge(all_dobs, 
+                     adoption_cleaned, 
+                     how = 'outer', 
+                     on = 'ID')
+
+
+#%%
+adoption_date_all_cleaned = adoption_date_all.copy().loc[adoption_date_all['SPECIES'] != 'Cat', :]
+# Coalesce columns that should be the same (i.e. 'ID #' and 'ID')
+adoption_date_all_cleaned['AGE MEASURE'] = adoption_date_all_cleaned['AGE MEASURE'].combine_first(adoption_date_all_cleaned['Age Measure'])
+adoption_date_all_cleaned['BREED'] = adoption_date_all_cleaned['BREED'].combine_first(adoption_date_all_cleaned['COAT PATTERN'])
+adoption_date_all_cleaned['BREED'] = adoption_date_all_cleaned['BREED'].combine_first(adoption_date_all_cleaned['MIX'])
+adoption_date_all_cleaned['BREED'] = adoption_date_all_cleaned['BREED'].combine_first(adoption_date_all_cleaned['PRIMARY BREED'])
+adoption_date_all_cleaned['BREED'] = adoption_date_all_cleaned['BREED'].combine_first(adoption_date_all_cleaned['Primary Breed'])
+adoption_date_all_cleaned['SECONDARY BREED'] = adoption_date_all_cleaned['SECONDARY BREED'].combine_first(adoption_date_all_cleaned['Secondary Breed'])
+adoption_date_all_cleaned['SPAYED/NEUTERED'] = adoption_date_all_cleaned['SPAYED/NEUTERED'].combine_first(adoption_date_all_cleaned['SPAYED/\nNEUTERED'])
+
+# Drop unneccessary columns
+adoption_date_all_cleaned = adoption_date_all_cleaned.drop(columns = ['Age Measure', 'COAT PATTERN', 'MIX',
+                                          'PRIMARY BREED', 'Primary Breed', 
+                                          'ADOPTER FIRST NAME', 'ADOPTER LAST NAME', 
+                                          'ADOPTION COORDINATOR', 'Address', 
+                                          'Cell Phone', 'City', 'E-mail', 'E-mail 1',
+                                          'FOSTER FIRST NAME', 'FOSTER LAST NAME',
+                                          'FOSTER NAME', 'Home Phone', 'LDAR Rep',
+                                          'MICROCHIP COMPANY', 'MICROCHIP ID', 
+                                          'Contract in ROVER', 'Info in ROVER',
+                                          'Intake Age (Months)', 'Intake Number',
+                                          'SPAYED/\nNEUTERED', 'in process',
+                                          'rescuegroups ID', 'scanned', 'Secondary Breed'
+                                          ]) 
+
+# Drop rows with no ID and duplicative IDs (there's only like 6, but still)
+adoption_date_all_cleaned = adoption_date_all_cleaned.loc[adoption_date_all_cleaned['ID'] != np.nan] \
+                                                    .drop_duplicates(subset = ['ID'])
+
+# Convert Date Adopted to datetime
+adoption_date_all_cleaned['DATE ADOPTED'] = adoption_date_all_cleaned['DATE ADOPTED'].fillna(pd.NaT).astype(np.datetime64, errors = 'ignore')
+adoption_date_all_cleaned['DATE ADOPTED'] = pd.to_datetime(adoption_date_all_cleaned['DATE ADOPTED'], errors = 'coerce')
+
+adoption_date_all_cleaned['BIRTHDATE'] = adoption_date_all_cleaned['BIRTHDATE'].fillna(pd.NaT).astype(np.datetime64, errors = 'ignore')
+adoption_date_all_cleaned['BIRTHDATE'] = pd.to_datetime(adoption_date_all_cleaned['BIRTHDATE'], errors = 'coerce')
+                       
+adoption_date_all_cleaned['ID'] = adoption_date_all_cleaned['ID'].str.strip()
+
+                             
+#%% Merge these new adoption and birth dates back in
+all_dates2 = all_dates.merge(adoption_date_all_cleaned[['ID', 'BIRTHDATE', 'DATE ADOPTED']],
+                             how = 'outer',
+                             on = 'ID')
+
+# Coalesce our adoption and birth dates
+all_dates2['Adopted On'] = all_dates2['Adopted On'].combine_first(all_dates2['DATE ADOPTED'])
+all_dates2['Date of Birth'] = all_dates2['Date of Birth'].combine_first(all_dates2['BIRTHDATE'])
+all_dates2 = all_dates2.drop(columns = ['DATE ADOPTED', 'BIRTHDATE']).drop_duplicates(subset = ['ID'])
+
+
+#%% Merge DOB and Date of Adoption back into our master adoption sheet
+master_dog_adoption = pd.merge(dog_list_deduped,
+                               all_dates2,
+                               on = 'ID',
+                               how = 'left')
+
+
 #%% Return a master dog list
-dog_list_deduped.to_csv('Data/Master_Adoption_List.csv', index = False)
+master_dog_adoption.to_csv('Data/Master_Adoption_List.csv', index = False)
